6ed658dd04d669370d930c50cf6ded893c5c3170
Yusuf Karaca <karacayif@gmail.com>
2025-09-05 10:45:39 +0200

    Fetch data stats done consolelog

--- src/pages/B-body/Statistic.tsx
+++ src/pages/B-body/Statistic.tsx
@@ -2,8 +2,27 @@ import Profil from "../../components/compoStat/Profil.tsx";
 import CardStatistic from "../../components/compoStat/CardStatistic.tsx";
 import CardMatch from "../../components/compoStat/CardMatch.tsx";
 import { Box } from "@mui/material";
+import { BASE_API_URL } from "../../../constants.ts";
+import axios from "axios";
+import { useEffect } from "react";
 
 const Statistic = () => {
+
+
+    const fetchDataStats = async () => {
+    try {
+        const response =await axios.get(BASE_API_URL + "/statistique");
+        console.log(response)
+    }
+    catch (error:any) {
+        console.log(error.response?.data || error.message);
+        alert("Le code d'accès est incorrect");
+    }
+    }
+
+    useEffect(() => {
+        fetchDataStats()
+    }, []);
 
     return (
         <Box sx={{ display: "flex", gap:15, p: 2 , justifyContent: "center" ,mb:9,mt:2}}>
