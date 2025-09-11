ad616969888186edd000817c18add031d21fdecd
yousra <yousra.italiana@gmail.com>
2025-09-11 13:44:13 +0200

    ok

--- src/pages/B-body/DashboardAgent.tsx
+++ src/pages/B-body/DashboardAgent.tsx
@@ -2,9 +2,27 @@ import { Container, Typography, Box } from "@mui/material";
 import AgendaCard from "../../components/compoDashboard/AgendaCard.tsx";
 import PlayersCarouselAgenda from "../../components/compoDashboard/PlayersCarouselAgenda.tsx";
 import TodoList from "../../components/compoDashboard/TodoList.tsx";
+import axios from "axios";
+import {useEffect, useState} from "react";
 
 
 const DashboardAgent = () => {
+
+    const [agentName, setAgentName] = useState<string>("");
+
+    useEffect(() => {
+        const fetchAgent = async () => {
+            try {
+                const response = await axios.get("http://localhost:8080/users/me");
+                setAgentName(response.data.name);
+            } catch (error) {
+                console.error( error);
+            }
+        };
+
+        fetchAgent();
+    }, []);
+
     return (
         <Container maxWidth="xl" sx={{ mt: 8}}>
             <Box
@@ -21,7 +39,7 @@ const DashboardAgent = () => {
                         variant="h3"
                         sx={{ color: "orange", fontWeight: "bold", mb: 3, ml:30}}
                     >
-                        BIENVENUE  FOREX
+                        Bienvenue {agentName || "Chargement..."}
                     </Typography>
 
                     <AgendaCard />
