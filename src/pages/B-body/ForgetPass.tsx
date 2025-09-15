77df2aefba4cfd21641280e45b891f6a76f13fda
yousra <yousra.italiana@gmail.com>
2025-09-15 14:59:04 +0200

    css page forgetPass

--- src/pages/B-body/ForgetPass.tsx
+++ src/pages/B-body/ForgetPass.tsx
@@ -9,27 +9,36 @@ const ForgetPass = () => {
     const navigate = useNavigate();
 
     return (
+    <div style={{
+        background: "linear-gradient(16deg, rgba(192, 110, 4, 1) 23%, rgba(246, 154, 3, 1) 48%, rgba(255, 244, 158, 1) 72%, rgba(255, 224, 178, 1) 100%)",
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        minHeight: "100vh",
+        width: "100%",
+        display: "flex", alignItems:"center",justifyContent:"center",flexDirection:"column"
+    }}>
     <Box sx={{ display: "flex", flexDirection: "column",
         justifyContent:"center",alignItems:"center",minHeight:"90vh" }}>
 
         <Typography
             variant="h5"
-            sx={{fontWeight: "bold", color: "#c06e04",}}
+            sx={{fontWeight: "bold", color: "#c06e04",mb:2}}
         >
             Mot de passe oublié
         </Typography>
 
         <Box
             sx={{
-                width: 562,
-                height: 622,
-                backgroundColor: "#FFE0B2",
+                width: 512,
+                height: 390,
+                backgroundColor: "#ffffff",
                 borderRadius: 2,
                 display: "flex",
                 flexDirection: "column",
                 alignItems: "center",
+                alignContent: "center",
                 pt: 4,
-                boxShadow:7,
+                boxShadow:2,
 
             }}
         >
@@ -37,20 +46,18 @@ const ForgetPass = () => {
                 src="/logo-dailyfoot.png"
                 alt="Logo DailyFoot"
                 style={{ width: 120, borderRadius: 70 }}
+                onClick={() => navigate("/Login")}
             />
 
-            <Box sx={{mt:6,display: "flex", alignItems: "center",gap: 4}}>
+            <Box sx={{mt:7,display: "flex", alignItems: "center",gap: 4}}>
                 <Input label="Email" name="username" type="text" />
             </Box>
-            <Box sx={{mt:10, display: "flex", alignItems: "center",gap: 4}}>
+            <Box sx={{mt:1, display: "flex", alignItems: "center",gap: 4}}>
                 <BtnLogin label="Valider" type="button" onClick={() => navigate("/Login")} />
-            </Box>
-            <Box sx={{mt:6, display: "flex", alignItems: "center",gap: 4}}>
-                <Button type="submit" onClick={() => navigate("/Login")} style={{color:"#f69a03"}} >Page de connexion</Button>
-
             </Box>
         </Box>
     </Box>
+    </div>
     );
 };
 
