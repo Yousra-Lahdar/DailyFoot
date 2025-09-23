70c042476037f7b8d34cecb9561590c0c982ad8d
yousra <yousra.italiana@gmail.com>
2025-09-23 16:04:22 +0200

    fotter

--- src/components/layout/Pages.tsx
+++ src/components/layout/Pages.tsx
@@ -1,6 +1,6 @@
 import type { ReactNode } from "react";
 import { Box } from "@mui/material";
-import FooterWithLogo from "../../pages/C-footer/FooterWithLogo.tsx"; // ou FooterWithoutLogo
+
 
 type PagesProps = {
     children: ReactNode;
@@ -18,7 +18,7 @@ const Pages = ({ children, title }: PagesProps) => {
         >
             <title>{title}</title>
             <Box sx={{ flex: 1 }}>{children}</Box>
-            <FooterWithLogo />
+
         </Box>
     );
 };
